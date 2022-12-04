8b0a1fda1abee36b9d975da60bd9bc8a13012d67
sara-p- <sarapitt88@gmail.com>
2022-12-04 00:16:53 -0500

    ticker and split text

--- src/js/modules/text.js
+++ src/js/modules/text.js
@@ -1,12 +1,14 @@
-export default function text() {
-  const fullscreenSections = document.querySelectorAll('.text--fullscreen')
+import gsap from 'gsap'
+import SplitText from 'gsap/SplitText'
+gsap.registerPlugin(SplitText)
 
-  // For each fullscreen text section, grab the words
-  fullscreenSections.forEach((section, index) => {
-    const sectionWords = section.querySelectorAll('.text__word')
-    // Now loop through the array of words, and set the font size to the height of the screen divided by the number of words
-    // sectionWords.forEach((word, index) => {
-    //   word.style.fontSize = `${100 / sectionWords.length - 5}vh`
-    // })
+export default function text() {
+  const theTexts = document.querySelectorAll('.text')
+  const split = new SplitText(theTexts, {
+    type: 'lines',
+    linesClass: 'text__line',
   })
+  // theTexts.forEach((text) => {
+
+  // })
 }
